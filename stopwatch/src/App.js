bebb09d205eb5b74e7b7c804757218c9480edb9e
Aayush-Rajagopalan <61064137+Aayush-Rajagopalan@users.noreply.github.com>
2022-03-07 19:10:33 +0300

    Stopwatch
    Made using Tauri and React

---
+++ stopwatch/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from './logo.svg'
+import tauriCircles from './tauri.svg'
+import tauriWord from './wordmark.svg'
+import './App.css'
+import StopWatch from './Components/Stopwatch/StopWatch.js'
+
+// javascript
+
+function App() {
+  return (
+    <div className="App">
+      <StopWatch />
+    </div>
+  )
+}
+
+export default App
